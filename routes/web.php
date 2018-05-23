326e3f2fc8e2fe0544619bdc560ffe6b55b30084
Outhwaite <81332101@qq.com>
2018-05-23 09:33:12 +0800

    账号激活

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,10 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 Route::resource('users', 'UsersController');
 
 Route::get('login', 'SessionsController@login')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
